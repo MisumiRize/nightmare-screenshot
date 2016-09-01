ee061f79a2beed5921f31650bc191a49d1446f0c
Misumi Rize <r@ayase-e.li>
2016-09-01 10:19:54 +0900

    support Node v4

--- index.js
+++ index.js
@@ -11,32 +11,50 @@ var screenshotRect = exports.screenshotRect = function (path, rect) {
     };
 };
 
-var screenshotSelector = exports.screenshotSelector = function(path, selector) {
+var screenshotSelector = exports.screenshotSelector = function(path, selector, cb) {
+    if (typeof cb != 'function') {
+        cb = () => { };
+    }
+
     return function(nightmare) {
+        function _screenshot(url, rect) {
+            nightmare.goto(url)
+                .screenshot(path, rect)
+                .run(cb);
+        }
+
+        function _evaluate(url) {
+            nightmare.goto(url)
+               .evaluate(function(_selector) {
+                    var _element = document.querySelector(_selector);
+                    if (_element) {
+                        var rect = _element.getBoundingClientRect();
+                        return {
+                            x: Math.round(rect.left),
+                            y: Math.round(rect.top),
+                            width: Math.round(rect.width),
+                            height: Math.round(rect.height)
+                        };
+                    }
+                }, selector)
+                .run((err, rect) => {
+                    if (err) {
+                        cb(err);
+                    } else if (!rect) {
+                        cb(new Error('invalid selector' + selector));
+                    } else {
+                        _screenshot(url, rect);
+                    }
+                });
+        }
+
         nightmare.url()
-            .then(function(url) {
-                nightmare.goto(url)
-                    .evaluate(function(_selector) {
-                        var _element = document.querySelector(_selector);
-                        if (_element) {
-                            var rect = _element.getBoundingClientRect();
-                            return {
-                                x: Math.round(rect.left),
-                                y: Math.round(rect.top),
-                                width: Math.round(rect.width),
-                                height: Math.round(rect.height)
-                            };
-                        }
-                    }, selector)
-                    .then(function(rect) {
-                        if (!rect) {
-                            throw new Error('invalid selector' + selector);
-                        } else {
-                            return nightmare.goto(url)
-                                .screenshot(path, rect)
-                                .end();
-                        }
-                    });
+            .run(function(err, url) {
+                if (err) {
+                    cb(err);
+                } else {
+                    _evaluate(url);
+                }
             });
     };
 };
